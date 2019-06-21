3f1dd43aecf53b5730871ba05e68d98e844c44f9
wby <646635653@qq.com>
2019-06-22 04:57:54 +0800

    加一个类

---
+++ src/main/java/com/wby/day2spring_mybatis/controller/Hisay.java
@@ -0,0 +1,16 @@
+package com.wby.day2spring_mybatis.controller;
+
+import com.sun.xml.internal.ws.message.source.PayloadSourceMessage;
+
+import javax.security.auth.kerberos.KerberosKey;
+
+/**
+ * @Author wangbangyun
+ * @Date 2019/6/22
+ * @Version 1.0
+ */
+public class Hisay {
+    public static void main(String[] args) {
+        System.out.println("kk");
+    }
+}
